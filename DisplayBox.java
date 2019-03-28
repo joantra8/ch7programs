ff8537e241d631e1b5965cf995680fc433e2e0d4
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-03-28 08:14:04 -0400

    Add files via upload

---
+++ DisplayBox.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DisplayBox {
+	public static void drawBar() {
+		Scanner input = new Scanner(System.in);
+		
+		double height;
+		System.out.print("Enter the height: ");
+		height = input.nextDouble();
+		
+	}
+
+}
