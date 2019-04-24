c74900203c6fac7c41003caa71ee4ba547538a53
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-04-24 07:56:36 -0400

    Update NimStrategy.java

--- NimStrategy.java
+++ NimStrategy.java
@@ -1,4 +1,31 @@
-
+import java.util.Scanner;
 public class NimStrategy {
+	public static void isValidEntry() {
+	}
+	public static void drawStones() {
+	}
+	public static void userTurn(int num) {
+		
+	}
+		
+		
+		
+	public static void main (String []args) {
+		int num = (int) (16 * Math.random() + 15);
+		
+		
+		while (num > 0) {
+			num = userTurn(num);
+			
+		if (num <= 0) {
+			System.out.print("You Win!");
+		}
+			
+		}
+
+
+		
+	}
+		
 
 }
