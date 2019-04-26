7f45e89d8f687a2aff399c8f4ed14587b912a3ab
joantra8 <45926552+joantra8@users.noreply.github.com>
2019-04-26 05:23:33 -0400

    Update PerfectIntegers.java

--- PerfectIntegers.java
+++ PerfectIntegers.java
@@ -1,17 +1,28 @@
 
 public class PerfectIntegers {
-	public static boolean isPerfect() {
-		return(true);
-		
+	public static boolean isPerfectNumber(int number) {
+		boolean p;
+		int temp = 0;
+		for (int i = 1; i <= number/2; i++) {
+			if (number % i == 0) {
+				temp += 1;
+			}
+		}
+	if (temp == number) {
+		p = true;
+		return p;
+	} else { 
+		p = false;
+		return p;
 	}
-	public static void main(String []args) {
-		int a, b, c;
-		for (a = 1; a < 100; a++) {
-		for (b = 1; b < 100; b++) {
-		for (c = 1; b < 100; c++) {
-			
-		}
-		}
+	}
+	public static void main (String []args) {
+		boolean p;
+		for (int i = 2; i <= 100; i++) {
+			p = isPerfectNumber(i);
+			if (p == true) {
+				System.out.println(i);
+			}
 		}
 	}
 
